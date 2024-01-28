c709c3759345dfb95f1c43767c5ed9d87af94e21
HAYDAWN <haydawn.@HAYDAWNs-iMac.local>
2024-01-28 01:21:09 +0000

    Added current city and 5 day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,8 +17,37 @@ $(document).ready(function () {
       fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
+          displayWeatherData(data);
         });
     }
+
+    function displayWeatherData(data) {
+      // Clear previous data
+      $("#today, #forecast").empty();
+  
+      // Display current weather
+      var currentWeather = $("<div>").addClass("current-weather");
+      currentWeather.append("<h2>" + data.name + "(" + new Date().toLocaleDateString()+ ")"+"</h2>");
+      currentWeather.append("<p>Temp: " + data.main.temp + " K</p>");
+      currentWeather.append("<p>Wind: " + data.wind.speed + " m/s</p>");
+      currentWeather.append("<p>Humidity: " + data.main.humidity + "%</p>");
+     
+  
+      $("#today").append(currentWeather);
+  
+      // Display 5-day forecast
+      var forecast = $("<div>").addClass("forecast");
+  
+      for (var i = 1; i <= 5; i++) {
+        var forecastDay = $("<div>").addClass("forecast-day");
+        forecastDay.append("<p>Date: " + new Date().toLocaleDateString() + "</p>");
+  
+        forecast.append(forecastDay);
+      }
+  
+      $("#forecast").append(forecast);
+    }
+    
   
     function addToSearchHistory(cityName) {
       $("#history").append("<div>" + cityName + "</div>");
